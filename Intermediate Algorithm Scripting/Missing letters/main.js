870248fe0255fafda2492ec445d7ca52f24203fd
lhuddlesto <lhuddlesto@gannett.com>
2018-11-20 22:10:49 -0500

    Completed missing letters challenge

--- Intermediate Algorithm Scripting/Missing letters/main.js
+++ Intermediate Algorithm Scripting/Missing letters/main.js
@@ -1,11 +1,23 @@
 function fearNotLetter(str) {
-    let first = str[0];
-    let last = str[str.length - 1]
-    console.log(first);
-    console.log(last);
-    let regex = new RegExp(/[first-last]/)
-    console.log(regex.match(str));
-    return str;
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    if (str == alphabet) {
+    return undefined
   }
-  
-  fearNotLetter("abce");
+  let alphabetSlice = alphabet.slice(alphabet.indexOf(str[0]), alphabet.indexOf(str[str.length - 1]));
+  console.log(alphabetSlice);
+ let sliceArr =  alphabetSlice.split("");
+ let strArr = str.split("");
+
+  let res = sliceArr.filter(e => {
+    for (let i = 0; i < strArr.length; i++) {
+      if (e === strArr[i]) {
+        return false;
+      }
+    }
+    return true
+  });
+  return res.join('');
+}
+
+fearNotLetter("stvwx");
